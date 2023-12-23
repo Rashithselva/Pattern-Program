a42fbdf7bc0c21d9bba294d19bfa434645f7a607
Rashithselva <ravirashith@gmail.com>
2023-12-23 18:22:32 +0530

    five line printed

--- Printstar.java
+++ Printstar.java
@@ -6,8 +6,12 @@ public class Printstar {
         int a=5;
 
         for(int i=1;i<=a;i++){
+             for(int j=1;j<=a;j++){
             System.out.print("*");
+         }
+         System.out.println( );
         }
+       
     }
     
 }
