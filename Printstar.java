c60422eadefad59297a7717a2f8cbeb9b8b93f3d
Rashithselva <ravirashith@gmail.com>
2023-12-23 18:26:28 +0530

    onother mothed

--- Printstar.java
+++ Printstar.java
@@ -6,12 +6,16 @@ public class Printstar {
         int a=5;
 
         for(int i=1;i<=a;i++){
-             for(int j=1;j<=a;j++){
-            System.out.print("*");
-         }
-         System.out.println( );
+            for(int j=1;j<=a;j++){
+                if(true){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.println(" ");
+                } 
+            }
+            System.out.println( );
         }
-       
     }
     
 }
