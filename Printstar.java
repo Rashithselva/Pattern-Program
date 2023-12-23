f7e003203b988a95752278ce2158856d828cc2be
Rashithselva <ravirashith@gmail.com>
2023-12-23 18:19:44 +0530

    one line printed

--- Printstar.java
+++ Printstar.java
@@ -1,5 +1,13 @@
 package Patternprogram;
 
 public class Printstar {
+    public static void main(String[] args) {
+        
+        int a=5;
+
+        for(int i=1;i<=a;i++){
+            System.out.print("*");
+        }
+    }
     
 }
